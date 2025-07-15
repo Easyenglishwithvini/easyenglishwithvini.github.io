b3ba55bb581e5f66b1f429e4e62f649cd23f9976
vinithaanup <vinithacse@gmail.com>
2025-07-15 07:56:47 +0200

    Remove Level-wise breakdown and change level test

---
+++ quizScriptNew.js
@@ -0,0 +1,289 @@
+var levels = {
+    part1: [
+             {
+               "Question": "__ your brother here? (Am/Are/Is/Does)",
+               "Answer": "Is"
+             },
+             {
+               "Question": "These are all __. (me/I/my/mine)",
+               "Answer": "mine"
+             },
+             {
+               "Question": "Where are my keys? I can't find ___. (them/him/it/they)",
+               "Answer": "them"
+             },
+             {
+               "Question": "__ X-ray (a or an)?",
+               "Answer": "an"
+             },
+             {
+               "Question": "__ unit (a or an)?",
+               "Answer": "a"
+             },
+             {
+               "Question": "___ are these pens? (Whose or Who's)",
+               "Answer": "Whose"
+             },
+             {
+               "Question": "വിദ്യാർത്ഥികളുടെ പരീക്ഷാഫലം വൈകുന്നു. The ____ exam results were delayed. (student's, student, students')",
+               "Answer": "students'"
+             },
+             {
+               "Question": "We are __ the signal. (on,at,in)",
+               "Answer": "at"
+             },
+             {
+               "Question": "The news is ___ TV (on,at,in)",
+               "Answer": "on"
+             },
+             {
+               "Question": "Fill in the correct word. അവൾ നിനക്ക് പണം വല്ലതും തന്നോ? Did she give you __ money?",
+               "Answer": "any"
+             },
+             {
+               "Question": "Fill in the correct WORDS. It’s my book. Can you _______ ? (അതെനിക്കു തരുമോ)",
+               "Answer": "give it to me?"
+             },
+             {
+               "Question": "Use the correct form of the verb clean- ഷെൽഫ് വൃത്തിയാക്കുന്നതിനിടയിൽ ഞാൻ ഗ്ലാസ് പൊട്ടിച്ചു. I broke the glass while __  the shelf",
+               "Answer": "cleaning"
+             },
+             {
+               "Question": "Use the correct form of the verb go- അവൻ വെള്ളിയാഴ്ച വീട്ടിൽ പോകാറുണ്ട്. He ___ home on Fridays",
+               "Answer": "goes"
+             },
+             {
+               "Question": "How many pots ____ every week? Use the verb make (അവൾ ആഴ്ചയിൽ എത്ര പാത്രങ്ങൾ ഉണ്ടാക്കാറുണ്ട് ?)",
+               "Answer": "does she make"
+             },
+             {
+               "Question": "How do you say this in English?  നിങ്ങൾ എപ്പോഴെങ്കിലും സിംഗപ്പൂരിൽ പോയിട്ടുണ്ടോ?",
+               "Answers": [
+                 "Have you ever been to Singapore?",
+                 "Have you been to Singapore?"
+               ]
+             },
+             {
+               "Question": "അവൾ എങ്ങനെ പോയി? How to say this in English?",
+               "Answer": "How did she go?"
+             },
+             {
+               "Question": "അവൻ എല്ലാ ദിവസവും ഓട്സ് കഴിക്കാറുണ്ട്. How do you say this in English?",
+               "Answers": [
+                 "He eats oats every day",
+                 "He eats oats daily"
+               ]
+             },
+             {
+               "Question": "Fill in the appropriate form of the word -bad. This is a bad book. That book is ____ (ആ പുസ്തകം അതിലും മോശമാണ്)",
+               "Answers": [
+                 "worse",
+                 "even worse"
+               ]
+             },
+             {
+               "Question": "Fill in the correct word(s). What ___ she eat? അവൾ എന്താ തിന്നാറു? ",
+               "Answer": "does"
+             },
+             {
+               "Question": "Fill in the correct article. He is ___ best doctor in this hospital",
+               "Answer": "the"
+             },
+             {
+               "Question": "നിനക്ക് മിണ്ടാതിരിക്കാൻ കഴിയുമോ? Can you ___ quiet?",
+               "Answer": "be"
+             }
+           ],
+    part2: [
+             {
+               "Question": "Fill in the correct form of verb go. She has ____ to the market",
+               "Answer":"gone"
+             },
+
+             {
+               "Question": "Choose the correct word (for, since). She has taken care of her ___ 2010",
+               "Answer":"since"
+             },
+
+             {
+               "Question": "അവൾക്കിപ്പോൾ പരീക്ഷ കഴിഞ്ഞിരിക്കണം. How to say this in English? Use a form of should. She _____ finished the exams by now",
+               "Answers": ["should have", "should've"]
+             },
+             {
+               "Question": "Add appropriate Question Tags: She had found a pen,___",
+               "Answers": ["hadn't she?", "had she not"]
+             },
+             {
+               "Question": "ഞാൻ എത്തിയപ്പോഴേക്കും അവർ പോയിരുന്നു. They _ when I arrived. (Use a form of leave)",
+               "Answer": "had left"
+             },
+             {
+               "Question": "Choose the correct word(s): (is, was, has been, is been).  She __ crying since morning",
+               "Answer": "has been"
+             },
+             {
+               "Question": "നിനക്ക് പോകാമായിരുന്നു. How to say this in English?",
+               "Answers": ["You could have gone", "You could've gone"]
+             },
+             {
+               "Question": " If I ____ rich, I would buy a big house. Choose the correct form of the verb 'to be'",
+               "Answer":"were"
+             },
+             {
+               "Question": "ഞാൻ ദിവസേന വ്യായാമം ചെയ്തിരുന്നു. Fill in the correct word(s) I ___ exercise regularly",
+               "Answer":"used to"},
+             {
+               "Question": "അവൾക്ക് ഇന്നലെ വരാൻ പറ്റിയില്ല. She ___ come yesterday. Choose the right word(s) (will not, should not, may not, could not)",
+               "Answers":["could not", "couldn't"]
+             },
+             {
+               "Question": "ഞാൻ അധികം കഴിക്കാൻ പാടില്ലായിരുന്നു? Write the correct option (shouldn't, couldn't, shouldn't have, couldn't have) I ____ eaten too much",
+               "Answers": ["shouldn't have", "should not have"]
+             },
+             {
+               "Question": "(Change to Reported Speech) Ali:\"I am tired\"",
+               "Answers":["Ali said that he was tired", "Ali said he was tired"]
+             },
+             {
+               "Question": "Choose the correct word(s) (should be cooking, should be cooked, should be cook).ഈ അരി 40-50 മിനിറ്റ് വേവിക്കണം  This rice ___ for 40 to 50 minutes.",
+               "Answer":"should be cooked"
+             },
+             {
+               "Question": "How do you say this in English? ഞാൻ എന്റെ കാർ നന്നാക്കിപ്പിച്ചു.",
+               "Answers":["I got my car repaired", "I had my car repaired"]
+             },
+             {
+               "Question": "Choose the correct word (make, get, let, have)  I can’t ___ him to wake up early. എനിക്ക് അവനെ നേരത്തെ ഉണർത്താൻ കഴിയില്ല.",
+               "Answer":"get"
+             }
+           ]
+};
+var state = {
+    progress: {
+        part1: 0,
+        part2: 0
+    },
+    score: {
+        part1: new Array(levels.part1.length).fill(0),
+        part2: new Array(levels.part2.length).fill(0)
+    },
+    transition: false
+};
+var maxMistakesPerLevel = 3;
+var noCourseNeeded = "If you have not guessed the answers, your basics are solid. All you need is some practice.I offer story based Speaking classes through through WhatsApp. They are not one to one classes. If interested contact +91 8157811691  "
+
+// Function to create a question div
+function createQuestionDiv(question) {
+  var questionDiv = document.createElement('div');
+  questionDiv.className = 'question';
+  questionDiv.appendChild(document.createTextNode(question));
+  return questionDiv;
+}
+
+// Function to update question and options
+function updateQuestionAndOptions(question, correctAnswer, level, callback) {
+    var questionContainer = document.getElementById('questionDiv');
+    questionContainer.innerHTML = '';
+    questionContainer.appendChild(createQuestionDiv(question));
+
+    document.getElementById('next').onclick = function() {
+        if (state.transition) {
+            return;
+        }
+        var answerContainer = document.getElementById('answer');
+        var answer = simplify(answerContainer.value);
+        if (correctAnswer instanceof Array) {
+            if (correctAnswer.findIndex(a => answer === simplify(a)) !== -1) {
+                incrementScore(level);
+                displayResult('✅');
+            } else {
+                displayResult('❌');
+            }
+        } else if (answer === simplify(correctAnswer)) {
+            incrementScore(level);
+            displayResult('✅');
+        } else {
+            displayResult('❌');
+        }
+        state.transition = true;
+        setTimeout(function () {
+            state.transition = false;
+            if (state.progress[level] < levels[level].length - 1) {
+                state.progress[level]++;
+                var nextQuestion = levels[level][state.progress[level]];
+                answerContainer.value = '';
+                clearResult();
+                updateQuestionAndOptions(nextQuestion.Question, nextQuestion.Answer || nextQuestion.Answers, level, callback);
+            } else {
+                if (hasFailedLevel(state.score[level])) {
+                    var scoreString = parseInt(Object.values(state.score)
+                                        .reduce((a, b) => a.concat(b))
+                                        .reduce((a,b) => a + b, ''), 2).toString(16);
+                    window.localStorage.setItem('levelTestScore', scoreString);
+                    var levelPage = window.open(`${level}.html`, '_self');
+                } else {
+                    clearResult();
+                    callback();
+                }
+            }
+        }, 1000);
+    };
+}
+
+function simplify(str) {
+    return str.trim().replace(/\s+/g, ' ').replace(/[.?]/g, '').replace(/[‘’]/g, "'").toLowerCase();
+}
+
+function incrementScore(level) {
+    state.score[level][state.progress[level]] = 1;
+}
+
+function displayResult(result) {
+    document.getElementById('result').innerHTML = result;
+}
+
+function clearResult() {
+    document.getElementById('result').innerHTML = '';
+}
+
+function hasFailedLevel(level) {
+    return level.length - level.reduce((a, b) => a + b) > maxMistakesPerLevel;
+}
+
+function startLevel(level, callback) {
+    document.getElementById('answer').value = '';
+    updateQuestionAndOptions(levels[level][0].Question, levels[level][0].Answer || levels[level][0].Answers, level, callback);
+}
+
+var keyDownListener = function(event) {
+    if (document.activeElement.id !== 'answer') {
+        document.getElementById('answer').focus();
+    }
+    if (event.keyCode == 13) {
+        if (!document.getElementById('intro').classList.contains('hidden')) {
+            document.getElementById('start-quiz').click();
+        }
+        if (document.getElementById('answer').value !== '') {
+            document.getElementById('next').click();
+        }
+    }
+};
+document.addEventListener('keydown', keyDownListener);
+
+document.getElementById('start-quiz').addEventListener('click', function(){
+    if (!document.getElementById('intro').classList.contains('hidden')) {
+        document.getElementById('quiz-container').classList.remove('hidden');
+        startLevel('part1', function() {
+            startLevel('part2', function() {
+                var questionContainer = document.getElementById('questionDiv');
+                questionContainer.innerHTML = '';
+                questionContainer.appendChild(createQuestionDiv(noCourseNeeded));
+                document.removeEventListener('keydown', keyDownListener);
+                document.getElementById('answer').classList.add('hidden');
+                document.getElementById('next').classList.add('hidden');
+                document.getElementById('enter-hint').classList.add('hidden');
+            });
+        });
+        document.getElementById('intro').classList.add('hidden');
+    }
+});
